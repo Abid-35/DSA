8eb605f4e080847f7439797d803d1c0c92b46b58
Abid Pathan <pabid4515@gmail.com>
2025-07-06 17:19:44 +0530

    DSA

--- binary.java
+++ binary.java
@@ -1,11 +1,12 @@
 
 
 public class binary{
+    static  int steps =0;
     public static void main(String[] args){
         System.out.println("Binary Search");
         int num [] ={2,4,6,8,10};
-        int target = 11;
-        int result = binarySearch(num,target);
+        int target = 10;
+        int result = binarySearch(num,target,0,num.length-1);
         if(result != -1){
             System.out.println("The element is found at :" + result);
         }
@@ -15,23 +16,26 @@ public class binary{
 
         
     }
-    public static int binarySearch(int num[],int target){
+    public static int binarySearch(int num[],int target,int left , int right){
         // 1 , 3 , 5 , 7 , 9
-        int left = 0;
-        int right = num.length-1;
-        while(left<=right){
+        // int left = 0;
+        // int right = num.length-1;
+        
+        if(left<=right){
             int mid = (left+right)/2;
+            steps++;
             if(num[mid] == target){
+                System.out.println("The no . of Steps are : " +steps);
                 return mid;
-
             }
-            else if(num[mid]<target){
-                left =mid+1;
-            }
-            else{
-                right = mid-1;
-            }
+            else if(num[mid]<target)
+               return binarySearch(num, target, mid+1, right);
+            
+            else
+               return  binarySearch(num, target, left, mid-1);
+            
         }
+        System.out.println("The no.of steps are : "+steps);
         return -1;
 
     }
