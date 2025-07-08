ebcf1ed50a4bbf528f20774d1869d56e7714a02b
Abid Pathan <pabid4515@gmail.com>
2025-07-08 18:11:38 +0530

    DSA

--- SelectionSort.java
+++ SelectionSort.java
@@ -2,29 +2,30 @@ public class SelectionSort {
     public static void main(String[] args) {
         System.out.println("Welcome to the world");
         int arr[] = {4, 2, 5, 1, 10, 9};
+        int minIndex = 0;
 
         System.out.println("Before Sorting:");
         for (int num : arr) {
             System.out.print(num + " ");
         }
+        System.out.println("");
+        System.out.println("Starting the Sorting..");
 
-        System.out.println("\nStarting the Sorting");
-
-        for (int i = 0; i < arr.length - 1; i++) {
-            int maxIndex = i;
-
-            for (int j = i + 1; j < arr.length - i; j++) {
-                if (arr[j] > arr[maxIndex]) {
-                    maxIndex = j;
-                }
+        for (int i = 0; i < arr.length-1; i++) {
+            minIndex = i;
+            for (int j = i+1; j < arr.length; j++) {
+                if(arr[minIndex]>arr[j])
+                  minIndex = j;  //By first iteration we will know that min index value is 1 at index 03 
             }
+            int temp = arr[minIndex];
+            arr[minIndex] =arr[i];
+            arr[i] =temp;
 
-            int last = arr.length - 1 - i;
-            int temp = arr[maxIndex];
-            arr[maxIndex] = arr[last];
-            arr[last] = temp;
+             for (int num : arr) {
+            System.out.print(num + " ");
+        }  
+        System.out.println(" "); 
         }
-
         System.out.println("Sorted Array:");
         for (int num : arr) {
             System.out.print(num + " ");
