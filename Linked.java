134aee04040ca2919e97e07a15e09b75286234d1
Abid Pathan <pabid4515@gmail.com>
2025-07-16 22:32:15 +0530

    DSA

--- Linked.java
+++ Linked.java
@@ -1,5 +1,14 @@
+import java.util.LinkedList;
+
 public class Linked{
     public static void main(String[] args){
         System.out.println("Trying the Linked List Methods ");
+        LinkedList Num = new LinkedList();
+        Num.add(5);
+        Num.add(6);
+        Num.addFirst(9);
+        System.out.println(Num.get(1));
+        System.out.println(Num);
+        System.out.println(Num.peek());
     }
 }
