4eded050d0912fad95c1b2260d0a97fad82527e2
Abid Pathan <pabid4515@gmail.com>
2025-07-16 22:36:37 +0530

    DSA

--- Linked.java
+++ Linked.java
@@ -4,11 +4,13 @@ public class Linked{
     public static void main(String[] args){
         System.out.println("Trying the Linked List Methods ");
         LinkedList Num = new LinkedList();
-        Num.add(5);
+        Num.add(5);//add method is used add the elemnts in linked list
         Num.add(6);
-        Num.addFirst(9);
-        System.out.println(Num.get(1));
+        Num.addFirst(9);//addfirst is used to add element at first 
+        System.out.println(Num.get(1));//used to access the element 
         System.out.println(Num);
-        System.out.println(Num.peek());
+        System.out.println(Num.peek());//to access the head element
     }
 }
+
+
