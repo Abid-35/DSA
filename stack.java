8687a160e17755a3bd3b653e46e7775c9473c952
Abid Pathan <pabid4515@gmail.com>
2025-07-18 19:51:07 +0530

    DSA

---
+++ stack.java
@@ -0,0 +1,14 @@
+import java.util.Stack;
+public class stack{
+    public static void main(String[] args) {
+
+        Stack num = new Stack();
+        num.push(30);
+        System.out.println("We are doing the pop " + num.pop());
+        num.push(40);
+        num.push(50);
+        num.push(60);
+        System.out.println("Stack"+num);
+        System.out.println("Peek gives last in element " + num.peek());
+        
+    }}
